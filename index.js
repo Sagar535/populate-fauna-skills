f38cdecd8d574da35d283c7e14cf79c43b797d87
Sagar Shah <070bct535@ioe.edu.np>
2022-02-13 13:36:42 +0545

    sort of uploads questions and answers... question and answers link not so clear

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const client = new faunadb.Client({
 	secret: 'fnAEcRVGlrACScAzR_KZBnX-5ntIR6EGcES2dVnT'
 })
 
+const fetch = require('node-fetch')
+
 const app = express()
 app.use(express.json())
 const PORT = process.env.PORt || 8000
@@ -68,6 +70,51 @@ app.post('/questions', async (req, res) => {
 		res.status(500).json({error: error.description})
 	}
 })
+
+
+app.get('/populate', async (req, res) => {
+	// fetch questions from skills asess api
+	const response = await fetch('https://skill-assess-api.vercel.app/api/questions/rubi-on-rails')
+	const questions = await response.json()
+
+
+	// console.log(questions)
+
+	// const createdQuestion = await client.query(
+	// 	q.Create(q.Collection('Question'), {
+	// 		data: {question: questions[0].question}
+	// 	})
+	// )
+
+	// console.log(createdQuestion.ref.id)
+
+	try {
+		questions.forEach(async (question) => {
+			const createdQuestion = await client.query(
+				q.Create(q.Collection('Question'), {
+					data: {question: question.question}
+				})
+			)
+
+			question.options.forEach((answer) => {
+				client.query(
+					q.Create(q.Collection('Answer'), {
+						data: {answer: answer.text, correct: answer.correct, owner: {connect: createdQuestion.ref.id}}
+					})
+				)
+			})
+		})
+
+		res.status(201).json({message: 'questions imported successfully'})
+	} catch (error) {
+		res.status(500).json({error: error.description})
+	}
+})
+
+// need it to easily populate and delete questions when necessary
+// delete all questions of particular topic
+
+
 
 
 app.listen(PORT, () => console.log(`Listening at port ${PORT}`))
